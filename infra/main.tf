e18fbb3c777778cdde864d0317fc61056d193ea3
shun198 <shunhiroseluvmri@gmail.com>
2023-01-03 17:09:32 +0900

    不要な記述を削除

--- infra/main.tf
+++ infra/main.tf
@@ -14,11 +14,3 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0bba69335379e17f8"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
